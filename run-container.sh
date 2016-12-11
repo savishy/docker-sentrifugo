dfe2c499b2779104d28b3f2984b7b05b84cb9b3d
Vish Venugopalakrishnan <savishy.pv@gmail.com>
2016-12-11 14:20:54 +0000

    fix #14 - docker composefile instead of shell scripts - deleted old shell scripts

--- run-container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run -d --name sentrifugo-db -e MYSQL_ROOT_PASSWORD=root mysql:5.7
-docker run -d -p 8080:8080 --name sentrifugo-app --link sentrifugo-db:sentrifugo-db docker-sentrifugo
